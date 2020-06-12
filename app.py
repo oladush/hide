130c16652aecc2ec439018c701f55c4618b7dcb8
olad <61180708+oladush@users.noreply.github.com>
2020-06-12 16:01:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from hide import parameters
 from hide.tools import aese, tools
 from hide.interface import form
 
-#test
+# time test
 import datetime
 
 # import parameters
@@ -17,30 +17,28 @@ error_count = parameters.error_count
 hidden_directory = parameters.dir_for_encryption
 
 
-
-
 def hide():
     offsets = list(tools.concat_files(tools.list_files(hidden_directory), 'file.bin', pub_key))
     tools.meta_outer(offsets)
-    shutil.rmtree(parameters.dir_for_encryption)
+    shutil.rmtree(parameters.dir_for_encryption, ignore_errors=True)
 
 
 def repay():
     os.mkdir(parameters.dir_for_encryption)
     tools.decode_bin(priv_key)
     meta = tools.meta_decode()
-    for i in meta:
-        tools.extract("file.bin", (i[0]), i[1], i[2])
+    tools.smart_extract("file_decode.bin", meta)
+    os.remove("file_decode.bin")
 
-#test
 
+aese.accept_key(pub_key, priv_key)
 
 if os.path.exists(hidden_directory) == True:
-    start_ = datetime.datetime.now() # test
+    start_ = datetime.datetime.now() # time test
     hide()
 else:
     password_status = form.main(password, error_count)
-    start_ = datetime.datetime.now() # test
+    start_ = datetime.datetime.now() # time test
     if password_status == True:
         repay()
     else:
