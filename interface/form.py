45373a20a0065991e93df58c6b2f580df93ee9c1
olad <61180708+oladush@users.noreply.github.com>
2020-07-29 20:33:40 +0900

    Update form.py

--- interface/form.py
+++ interface/form.py
@@ -6,56 +6,65 @@ pyglet.font.add_file("interface/font/circula-medium.otf")
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
 
-def main(password_true, error_count):
-    password_status = False
+class PasswordWindow():
+    """This class describe logic and appearance window for enter password"""
 
-    def enter(event):
-        nonlocal label, password_status, password_true, error_count
-        password = form.get()
-        if password == password_true:
-            error_count = 5
-            password_status = True
-            root.destroy()
+    def __init__(self, password_true, attempt_count):
+        self.password_stat = False
+        self.password_true = password_true
+        self.attempt_count = attempt_count
+
+    def enter(self, event):
+        password = self.form.get()
+        if password == self.password_true:
+            self.password_stat = True
+            self.root.destroy()
         else:
-            error_count -= 1
-            text = 'permission denied: ' + str(error_count)
-            error_text = tkinter.Label(root, text=text, font='circula-medium 12', fg="red", bg="black")
+            self.attempt_count -= 1
+            text = "permission denied: " + str(self.attempt_count)
+            error_text = tkinter.Label(self.root, text=text, font=self.principal_font, fg=self.font_error_color, bg=self.background_color)
             error_text.place(relx=.5, rely=.75, anchor="center")
 
-        if error_count <= 0:
-            block()
-            password_status = False
+            if self.attempt_count <= 0:
+                self.block()
 
-    def block():
-        nonlocal label, form
-        try:
-            label.destroy()
-            form.destroy()
-        except NameError:
-            pass
-        label = tkinter.Label(root, text="ooops", font="circula-medium 25", fg="white", bg="black", relief="flat")
-        label.place(relx=.5, rely=.4, anchor="center")
+    def run(self):
+
+        self.root.title("")
+        self.root.geometry("600x300")
+        self.root.iconbitmap("interface/icon/lock.ico")
+        self.root.configure(background=self.background_color)
+        self.root.resizable(width=False, height=False)
+
+        if self.attempt_count > 0:
+            self.label = tkinter.Label(self.root, text="enter password", font=self.principal_font + "25", fg=self.font_color, bg=self.background_color, relief=self.relief_design)
+            self.label.place(relx=.5, rely=.4, anchor="center")
+
+            self.form = tkinter.Entry(self.root, font='Arial 22', show="•", relief=self.relief_design, justify="center")
+            self.form.place(relx=.5, rely=.60, anchor="center", width=250, height=30)
+            self.form.focus()
+
+            self.form.bind("<Return>", self.enter)
+        else:
+            self.block()
+        self.root.mainloop()
+        return self.password_stat
+
+    def block(self):
+        self.label.destroy()
+        self.form.destroy()
+
+        self.label = tkinter.Label(self.root, text="ooops", font=self.principal_font + "25", fg=self.font_color, bg=self.background_color, relief=self.relief_design)
+        self.label.place(relx=.5, rely=.4, anchor="center")
 
     root = tkinter.Tk()
 
-    root.title("")
-    root.geometry("600x300")
-    root.iconbitmap("interface/icon/lock.ico")
-    root.configure(background='black')
-    root.resizable(width=False, height=False)
+    # внешний вид
 
-    if error_count > 0:
-        label = tkinter.Label(root, text="enter password", font="circula-medium 25", fg="white", bg="black",
-                              relief="flat")
-        label.place(relx=.5, rely=.4, anchor="center")
+    font_color = "white"
+    font_error_color = "red"
+    principal_font = "circula-medium "
+    background_color = "black"
+    relief_design = "flat"
 
-        form = tkinter.Entry(root, font='Arial 22', show="•", relief="flat", justify="center")
-        form.place(relx=.5, rely=.60, anchor="center", width=250, height=30)
-        form.focus()
 
-        form.bind("<Return>", enter)
-    else:
-        block()
-
-    root.mainloop()
-    return password_status
